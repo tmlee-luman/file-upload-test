13e2734955c15d840c18f88f5760ae320fe35482
tmlee <tmlee@shinsungtk.com>
2022-03-30 18:57:36 +0900

    file

--- src/main/java/com/test/fileuploadtest/controller/FileUploadController.java
+++ src/main/java/com/test/fileuploadtest/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,7 +19,7 @@ import java.io.IOException;
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public ResponseEntity<String> upload(MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
         String fileName = FileNameMaker.getName(UploadPath.value, multipartFile.getOriginalFilename());
         multipartFile.transferTo(new File(fileName));
 
