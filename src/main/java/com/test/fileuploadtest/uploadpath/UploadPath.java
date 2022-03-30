0873d4a8f005121ccf19c3393b1f0ecd6fd64d3a
tmlee <tmlee@shinsungtk.com>
2022-03-30 19:06:39 +0900

    경로 수정

--- src/main/java/com/test/fileuploadtest/uploadpath/UploadPath.java
+++ src/main/java/com/test/fileuploadtest/uploadpath/UploadPath.java
@@ -2,5 +2,5 @@ package com.test.fileuploadtest.uploadpath;
 
 public class UploadPath {
 
-    public static final String value = "/home/test_file/";
+    public static final String value = "/home/ubuntu/test_file/";
 }
