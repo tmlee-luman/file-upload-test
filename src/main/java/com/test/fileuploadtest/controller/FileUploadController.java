f0bfcbeafbb819415070c3de4407c99cd979c7bd
tmlee <tmlee@shinsungtk.com>
2022-04-06 15:29:19 +0900

    test 추가

--- src/main/java/com/test/fileuploadtest/controller/FileUploadController.java
+++ src/main/java/com/test/fileuploadtest/controller/FileUploadController.java
@@ -5,25 +5,43 @@ import com.test.fileuploadtest.uploadpath.UploadPath;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
 @Slf4j
-@RequestMapping("/api/v1")
 @RestController
 public class FileUploadController {
 
-    @PostMapping("/upload")
+    @PostMapping("/api/v1/upload")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
         String fileName = FileNameMaker.getName(UploadPath.value, multipartFile.getOriginalFilename());
         multipartFile.transferTo(new File(fileName));
 
         return ResponseEntity.ok()
                 .body(fileName);
+    }
+
+    @PostMapping("/api/v2/upload")
+    public ResponseEntity<String> upload(HttpServletRequest request, @RequestBody MultipartFile multipartFile) {
+        String contentRange = request.getHeader("Conetent-Range");
+        int rangeSeparator= contentRange.indexOf("-");
+        int sizeSeparator= contentRange.indexOf("/");
+
+        String rangeStart = contentRange.substring(0, rangeSeparator);
+        String rangeEnd = contentRange.substring(rangeSeparator, sizeSeparator);
+        String size = contentRange.substring(sizeSeparator);
+
+        log.trace("range start: {}", rangeStart);
+        log.trace("range end: {}", rangeEnd);
+        log.trace("range size: {}", size);
+        log.trace("file: {}", multipartFile.getOriginalFilename());
+        return ResponseEntity.ok()
+                .build();
     }
 }
